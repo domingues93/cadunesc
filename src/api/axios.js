14469990ce22f3ff8925ee2acae9bd41dc780117
domingues93 <glederson.r.o@gmail.com>
2020-10-23 17:51:13 -0300

    adicionado https

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export default axios.create({
     //baseURL: "https://cadunesc.diego-gomes.com.br/api",
-    baseURL: "http://cadunesc.domingues.xyz/api",
+    baseURL: "https://cadunesc.domingues.xyz/api",
     headers: {
         "Content-Type": "application/json; charset=UTF-8"
     }
