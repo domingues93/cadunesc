adad154bb2cd5d13b27e4a05df8016a0aea03067
domingues93 <glederson.r.o@gmail.com>
2020-10-23 12:29:20 -0300

    adicionado arquivo de deploy.

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    name: "cadunesc-panel",
+    script: 'npx',
+    args: "serve -s build -l 9002"
+  }],
+
+  deploy : {
+    production : {
+      user : 'domingues',
+      host : '149.56.84.0',
+      ref  : 'origin/master',
+      repo : 'git@github.com:domingues93/cadunesc.git',
+      path : '/home/domingues/cadunesc/web-panel',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npx && npm install && npm run build && pm2 startOrRestart ecosystem.config.js',
+      'pre-setup': ''
+    }
+  }
+};
